39636e6c1f9c19a6e2644bb70d50ca9022a8d084
hg-harshita <harshitaginti00444@gmail.com>
2025-04-28 12:06:36 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-//import express from "express"
-//import cors from 'cors'
+import express from "express"
+import cors from 'cors'
 import 'dotenv/config' 
 import connectDB from "./configs/mongodb.js"
 import { clerkWebhooks } from "./controllers/webhooks.js"
 
 //Initialize Express
-const express = require('express')
-const cors = require('cors') 
-const app = express()
+
+const app = express();
 
 //Connect to database
-await connectDB()
+connectDB()
 
 //Middlewares
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //Routes
 app.get('/', (req, res) => res.send("API Working"))
